79df83f68d4a6b8da12b4a42421d7ad9ee59b615
rathorejaimalbanna <145917781+rathorejaimalbanna@users.noreply.github.com>
2024-05-29 08:50:15 +0530

    phase 3

--- src/Components/pages/bookModal.jsx
+++ src/Components/pages/bookModal.jsx
@@ -1,58 +1,54 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { Button, Dropdown, DropdownButton } from "react-bootstrap";
-import styles from "./pages.module.css"
-import { doc, setDoc, updateDoc } from "firebase/firestore";
+import styles from "./pages.module.css";
+import { addDoc, collection, doc, setDoc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { userSelector } from "../../Redux/userReducer/userReducer";
 import { projectActions } from "../../Redux/projectReducer/projectReducer";
 
-
-
 export default function BookModal(props) {
-  const [name,setName] = useState("");
-  const [contact,setContact] = useState("")
-  const [offer,setOffer] = useState()
-  const [addhar,setAddhar] = useState()
-  const [mode,setMode] = useState("cash")
-  const {user} = useSelector(userSelector)
-  const [utr,setUtr] = useState("")
-  const dispatch = useDispatch()
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [offer, setOffer] = useState();
+  const [addhar, setAddhar] = useState();
+  const [mode, setMode] = useState("cash");
+  const { user } = useSelector(userSelector);
+  const [utr, setUtr] = useState("N/A");
+  const dispatch = useDispatch();
 
-  function handleSelect(eventKey)
-  {
-    setMode(eventKey)
-  };
+  function handleSelect(eventKey) {
+    setMode(eventKey);
+  }
   async function updatePlot(array) {
     const frankDocRef = doc(db, "projects", props.project.name);
     await updateDoc(frankDocRef, {
       plots: array,
     });
   }
-  async function addBooking()
-  {
-    await setDoc(doc(db, "Booking Request",user.email ), {
+  async function addBooking() {
+    await addDoc(collection(db, "Booking Request"), {
       name,
       contact,
       addhar,
       mode,
       offer,
-      plot:props.plotDetail.id,
-      email:user.email,
-      project:props.project.name
-      });
+      utr,
+      plot: props.plotDetail.id,
+      email: user.email,
+      project: props.project.name,
+    });
   }
-  async function handleSubmit(e)
-  {
-    e.preventDefault()
-    alert("Booking Successfull")
-    addBooking()
-      var plotsObject = [...props.project.plots];
+  async function handleSubmit(e) {
+    e.preventDefault();
+    alert("Booking Successfull");
+    addBooking();
+    var plotsObject = [...props.project.plots];
     plotsObject[props.plotDetail.arrId] = {
       id: props.plotDetail.id,
       status: "booked",
-      price:props.plotDetail.price? props.plotDetail.price:0,
-      area:props.plotDetail.area? props.plotDetail.area:0
+      price: props.plotDetail.price ? props.plotDetail.price : 0,
+      area: props.plotDetail.area ? props.plotDetail.area : 0,
     };
     updatePlot(plotsObject);
     dispatch(
@@ -60,39 +56,90 @@ export default function BookModal(props) {
         plot: plotsObject,
         plotId: props.plotId,
         projectId: props.projectId,
-      }))
-    props.handleClose()
+      })
+    );
+    props.handleClose();
   }
   return (
     <>
-    <div>
-      <h2>Book This Plot</h2>
-      <form onSubmit={handleSubmit} style={{marginTop:"20px"}}>
-        <h5>Enter Name</h5>
-        <input required onChange={(e)=> setName(e.target.value)} type="text" placeholder='Your Name' className={styles.inputFieldModal} />
-        <h5>Enter contact number</h5>
-        <input onChange={(e)=> setContact(e.target.value)} type="number" placeholder='Contact No.' className={styles.inputFieldModal} />
-        <h5>Select Payment Mode</h5>
-        <DropdownButton
-          id="dropdown-button"
-          title={`Select Payment Method`}
-          onSelect={handleSelect}
+      <div>
+        <h2>Book This Plot</h2>
+        <form onSubmit={handleSubmit} style={{ marginTop: "20px" }}>
+          <h5>Enter Name</h5>
+          <input
+            required
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Your Name"
+            className={styles.inputFieldModal}
+          />
+          <h5>Enter contact number</h5>
+          <input
+            onChange={(e) => setContact(e.target.value)}
+            type="number"
+            placeholder="Contact No."
+            className={styles.inputFieldModal}
+          />
+          <h5>Select Payment Mode</h5>
+          <DropdownButton
+            id="dropdown-button"
+            title={`Select Payment Method`}
+            onSelect={handleSelect}
           >
-          <Dropdown.Item eventKey="Bank">Bank</Dropdown.Item>
-          <Dropdown.Item eventKey="Cash">Cash</Dropdown.Item>
-          <Dropdown.Item eventKey="Cheque">Cheque</Dropdown.Item>
-        </DropdownButton>
-        {mode === "Bank" && <> <h5>Provide UTR number</h5>
-        <input placeholder="Utr number" type="number" onChange={(e)=>setUtr(e.target.value)} /></>}
+            <Dropdown.Item eventKey="Bank">Bank</Dropdown.Item>
+            <Dropdown.Item eventKey="Cash">Cash</Dropdown.Item>
+            <Dropdown.Item eventKey="Cheque">Cheque</Dropdown.Item>
+          </DropdownButton>
+          {mode === "Bank" && (
+            <>
+              {" "}
+              <h5>Provide UTR number</h5>
+              <input
+                placeholder="Utr number"
+                type="number"
+                onChange={(e) => setUtr(e.target.value)}
+              />
+            </>
+          )}
           <h5>Booking Amount</h5>
-        <input required onChange={(e)=> setOffer(e.target.value)} type="number" placeholder='ACC. NO.' className={styles.inputFieldModal}/>
-        <h5>Provide Addhar Number</h5>
-        <input required onChange={(e)=> setAddhar(e.target.value)} type="number" placeholder='Addhar NO.' className={styles.inputFieldModal}/>
-       <div> <Button type="submit" style={{display:'inline'}} className={styles.submitButton} >Submit</Button>
-      <Button variant="danger" style={{display:"inline",marginTop:"20px",marginLeft:"20px"}} onClick={props.handleClose}>Close</Button>
+          <input
+            required
+            onChange={(e) => setOffer(e.target.value)}
+            type="number"
+            placeholder="ACC. NO."
+            className={styles.inputFieldModal}
+          />
+          <h5>Provide Addhar Number</h5>
+          <input
+            required
+            onChange={(e) => setAddhar(e.target.value)}
+            type="number"
+            placeholder="Addhar NO."
+            className={styles.inputFieldModal}
+          />
+          <div>
+            {" "}
+            <Button
+              type="submit"
+              style={{ display: "inline" }}
+              className={styles.submitButton}
+            >
+              Submit
+            </Button>
+            <Button
+              variant="danger"
+              style={{
+                display: "inline",
+                marginTop: "20px",
+                marginLeft: "20px",
+              }}
+              onClick={props.handleClose}
+            >
+              Close
+            </Button>
+          </div>
+        </form>
       </div>
-     </form>
-    </div>
     </>
   );
 }
--- src/Components/pages/projectDetails.jsx
+++ src/Components/pages/projectDetails.jsx
@@ -8,72 +8,102 @@ import PlotDetails from "./plotDetails";
 
 export default function ProjectDetails() {
   const params = useParams();
-  const [show,setShow] = useState(false);
-  const [plotInfo,setPlotInfo] = useState(false);
-  const [plotModalDetail,setModalDetail] = useState({})
+  const [show, setShow] = useState(false);
+  const [plotInfo, setPlotInfo] = useState(false);
+  const [plotModalDetail, setModalDetail] = useState({});
   const loadedProjects = useSelector(projectSelectors);
   const project = loadedProjects[params.id];
-  function handleShow()
-  {
-    setShow(true)
-    setPlotInfo(false)
+  function handleShow() {
+    setShow(true);
+    setPlotInfo(false);
   }
-  function handleClose()
-  {
-    setShow(false)
-  };
-  function handleCloseInfo()
-  {
-    setPlotInfo(false)
+  function handleClose() {
+    setShow(false);
   }
-  function showInfo(info)
-  { setModalDetail(info)
-    setPlotInfo(true)
+  function handleCloseInfo() {
+    setPlotInfo(false);
+  }
+  function showInfo(info) {
+    setModalDetail(info);
+    setPlotInfo(true);
   }
 
   if (project) {
     return (
       <>
-      {plotInfo && <div className={styles.modalContainer}> <div className={styles.modalDiv}>
-      <PlotDetails handleCloseInfo={handleCloseInfo} handleShow={handleShow} plotDetail={plotModalDetail} projectName={project.name} projectLocation={project.location}  handleClose={handleClose}  />
-
-    </div></div>}
-      {show && <div className={styles.modalContainer}> <div className={styles.modalDiv}>
-      <BookModal projectId={params.id} project={project} plotDetail={plotModalDetail}  handleClose={handleClose}  />
-
-    </div></div>}
+        {plotInfo && (
+          <div className={styles.modalContainer}>
+            {" "}
+            <div className={styles.modalDiv}>
+              <PlotDetails
+                handleCloseInfo={handleCloseInfo}
+                handleShow={handleShow}
+                plotDetail={plotModalDetail}
+                projectName={project.name}
+                projectLocation={project.location}
+                handleClose={handleClose}
+              />
+            </div>
+          </div>
+        )}
+        {show && (
+          <div className={styles.modalContainer}>
+            {" "}
+            <div className={styles.modalDiv}>
+              <BookModal
+                projectId={params.id}
+                project={project}
+                plotDetail={plotModalDetail}
+                handleClose={handleClose}
+              />
+            </div>
+          </div>
+        )}
         <div>
           <h1 style={{ marginTop: "25px", color: "rgb(13,110,253)" }}>
             {project.name}
           </h1>
           <img className={styles.projectImg} src={project.image} alt="" />
-          <div style={{marginTop:"25px"}}>
+          <div style={{ marginTop: "25px" }}>
             <h3>Indications: </h3>
-            <div style={{display:"flex",justifyContent:"space-evenly"}}>
-            <p>
-              <span
-                className={styles.indiSpan}
-                style={{ backgroundColor: "lightgreen" }}
-              ></span>Available
-            </p>
-            <p>
-              <span
-                className={styles.indiSpan}
-                style={{ backgroundColor: "red" }}
-              ></span>Sold
-            </p>
-            <p>
-              <span
-                className={styles.indiSpan}
-                style={{ backgroundColor: "yellow" }}
-              > </span>Booked
-            </p>
+            <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+              <p>
+                <span
+                  className={styles.indiSpan}
+                  style={{ backgroundColor: "lightgreen" }}
+                ></span>
+                Available
+              </p>
+              <p>
+                <span
+                  className={styles.indiSpan}
+                  style={{ backgroundColor: "red" }}
+                ></span>
+                Sold
+              </p>
+              <p>
+                <span
+                  className={styles.indiSpan}
+                  style={{ backgroundColor: "yellow" }}
+                >
+                  {" "}
+                </span>
+                Booked
+              </p>
             </div>
           </div>
           <div className={styles.plotsDiv}>
-            {project.plots.map((item,arrId) => (
+            {project.plots.map((item, arrId) => (
               <div
-              onClick={()=>showInfo({price:item.price,area:item.area,id:item.id,arrId:arrId,status:item.status})}
+                onClick={() =>
+                  showInfo({
+                    price: item.price,
+                    area: item.area,
+                    id: item.id,
+                    arrId: arrId,
+                    status: item.status,
+                  })
+                }
                 className={styles.plotDiv}
                 style={{
                   backgroundColor:
